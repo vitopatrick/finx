77fa4561d2fea14e6618b0fe132b17031a54e0fb
warlock <fecypher@gmail.com>
2024-08-11 21:01:25 +0100

    change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-import Welcome from "../components/dashboard/Welcome";
+// import Welcome from "../components/dashboard/Welcome";
 import Prices from "../components/dashboard/Prices";
 import TradingViews from "../components/dashboard/TradingViews";
 
 const Dashboard = () => {
   return (
     <Layout>
-      <Welcome />
+      {/* <Welcome /> */}
       <Prices />
       <TradingViews />
     </Layout>
